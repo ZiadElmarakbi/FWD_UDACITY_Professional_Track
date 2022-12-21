c72e17f17e15317d099fa401c8efbc78b7a0043c
ZiadElmarakbi <zezoalbendary20@gmail.com>
2022-12-21 11:29:47 +0300

    Adding Project Files

---
+++ Payment Application C Project/Payment Application/Application/Application.h
@@ -0,0 +1,18 @@
+/*
+ * Application.h
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: asus
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+typedef enum {
+	NO_ERROR, ERROR
+}FUNC_STATE;
+
+void appStart(void);
+void ATM_WELCOME(void);
+
+#endif /* APPLICATION_H_ */
---
+++ Payment Application C Project/Payment Application/Terminal/Terminal.h
@@ -0,0 +1,36 @@
+/*
+ * Terminal.h
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: asus
+ */
+
+#ifndef TERMINAL_H_
+#define TERMINAL_H_
+
+#include "../Card/Card.h"
+
+typedef struct ST_terminalData_t
+{
+	float transAmount;
+	float maxTransAmount;
+	uint8_t transactionDate[11];
+
+}ST_terminalData_t ;
+
+typedef enum EN_terminalError_t
+{
+     TERMINAL_OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_AMOUNT,
+	 EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
+
+}EN_terminalError_t ;
+
+// APIs
+EN_terminalError_t getTransactionDate(ST_terminalData_t *termData);
+EN_terminalError_t isCardExpired(ST_cardData_t *cardData, ST_terminalData_t *termData);
+EN_terminalError_t getTransactionAmount(ST_terminalData_t *termData);
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t *termData);
+EN_terminalError_t setMaxAmount(ST_terminalData_t *termData, float maxAmount);
+EN_terminalError_t isValidCardPAN(ST_cardData_t *cardData); // Optional
+
+#endif /* TERMINAL_H_ */
